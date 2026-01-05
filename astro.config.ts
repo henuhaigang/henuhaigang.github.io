c89a18cd9b0089b74b08c7bdac84fbd06dcbe3a2
scorpion <henuhaigang@163.com>
2026-01-05 17:01:43 +0800

    UDPATE

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://henuhaigang.github.io",
-  base: "/",
+  base: "/my-blog/",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
